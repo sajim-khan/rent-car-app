dd74231cb547bb5874b1e01a86424dda255c5c26
sajim-khan <shossain1427@gmail.com>
2023-09-01 23:04:36 +0600

    added about section

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import About from '@/Components/About'
 import Hero from '@/Components/Hero'
 import Image from 'next/image'
 
@@ -5,6 +6,7 @@ export default function Home() {
   return (
     <main className="overflow-hidden">
     <Hero></Hero>
+    <About></About>
     </main>
   )
 }
