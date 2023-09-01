e8b8da31ab0b8c3f46320b0fb3d39a1f62ddf755
sajim-khan <shossain1427@gmail.com>
2023-09-01 23:02:53 +0600

    added hero component

---
+++ Components/Hero.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Hero = () => {
+    return (
+        <div>
+            <p>This is hero page</p>
+            <p>This is hero page</p>
+            <p>This is hero page</p>
+            <p>This is hero page</p>
+            <p>This is hero page</p>
+            <p>This is hero page</p>
+        </div>
+    );
+};
+
+export default Hero;
