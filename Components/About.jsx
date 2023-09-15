6f662a3ade853242c73b314a2a848996e30404fc
sajim-khan <shossain1427@gmail.com>
2023-09-15 22:24:26 +0600

    added

--- Components/About.jsx
+++ Components/About.jsx
@@ -4,6 +4,11 @@ const About = () => {
     return (
         <div>
             <h1>This is about page</h1>
+            <h1>This is about page</h1>
+            <h1>This is about page</h1>
+            <h1>This is about page</h1>
+            <h1>This is about page</h1>
+            <h1>This is about page</h1>
         </div>
     );
 };
