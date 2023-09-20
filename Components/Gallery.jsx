322cd39e110da4c0c771d581750c9d8f9a588e5a
sajim-khan <shossain1427@gmail.com>
2023-09-20 22:45:24 +0600

    added gallery image

--- Components/Gallery.jsx
+++ Components/Gallery.jsx
@@ -6,44 +6,44 @@ const Gallery = () => {
         <section className="py-6 dark:bg-gray-800 dark:text-gray-50">
           <div className="container grid grid-cols-2 gap-4 p-4 mx-auto md:grid-cols-4">
             <img
-              src="https://images.carexpert.com.au/resize/3000/-/app/uploads/2023/02/2023-Mitsubishi-Pajero-Sport-GSR-HERO-16x9-1.jpg"
+              src="https://parkers-images.bauersecure.com/Scale/pagefiles/315233/manufacturers/900x600/audi.jpg"
               alt=""
               className="w-full h-full col-span-2 row-span-2 rounded shadow-sm min-h-96 md:col-start-3 md:row-start-1 dark:bg-gray-500 aspect-square"
             />
             <img
-              src="https://imgd.aeplcdn.com/664x374/ec/b8/0e/9739/img/m/Mitsubishi-Pajero-Sport-Right-Front-Three-Quarter-52939_ol.jpg?v=201711021421&q=80&q=80"
+              src="https://images.pexels.com/photos/11139552/pexels-photo-11139552.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
               alt=""
               className="w-full h-full rounded shadow-sm min-h-48 dark:bg-gray-500 aspect-square"
             />
             <img
               alt=""
               className="w-full h-full rounded shadow-sm min-h-48 dark:bg-gray-500 aspect-square"
-              src="https://source.unsplash.com/random/200x200/?1"
+              src="https://static1.hotcarsimages.com/wordpress/wp-content/uploads/2021/08/Mercedes-SLR-McLaren-Mansory.jpg?q=50&fit=contain&w=1140&h=&dpr=1.5"
             />
             <img
               alt=""
               className="w-full h-full rounded shadow-sm min-h-48 dark:bg-gray-500 aspect-square"
-              src="https://source.unsplash.com/random/200x200/?2"
+              src="https://cdni.autocarindia.com/Utils/ImageResizer.ashx?n=https://cms.haymarketindia.net/model/uploads/modelimages/BMW-2-Series-Gran-Coupe-271220221147.jpg&w=373&h=245&q=75&c=1"
             />
             <img
               alt=""
               className="w-full h-full rounded shadow-sm min-h-48 dark:bg-gray-500 aspect-square"
-              src="https://source.unsplash.com/random/200x200/?3"
+              src="https://cdni.autocarindia.com/Utils/ImageResizer.ashx?n=https://cms.haymarketindia.net/model/uploads/modelimages/Audi-A4-190120211207.jpg&w=373&h=245&q=75&c=1"
             />
             <img
               alt=""
               className="w-full h-full rounded shadow-sm min-h-48 dark:bg-gray-500 aspect-square"
-              src="https://source.unsplash.com/random/200x200/?4"
+              src="https://i.pinimg.com/originals/ce/b0/0e/ceb00ec87ccc408b1626112c1657815a.jpg"
             />
             <img
               alt=""
               className="w-full h-full rounded shadow-sm min-h-48 dark:bg-gray-500 aspect-square"
-              src="https://source.unsplash.com/random/200x200/?5"
+              src="https://bgr.com/wp-content/uploads/2017/11/tesla-roadster.jpg?quality=82&strip=all&w=1000&h=574&crop=1"
             />
             <img
               alt=""
               className="w-full h-full rounded shadow-sm min-h-48 dark:bg-gray-500 aspect-square"
-              src="https://source.unsplash.com/random/200x200/?6"
+              src="https://www.audiusa.com/content/dam/nemo/ca/NoMo/Home/730x730/883x883_MY24---Q4-e-tron---Side-Profile---Driving.jpg"
             />
             <img
               alt=""
